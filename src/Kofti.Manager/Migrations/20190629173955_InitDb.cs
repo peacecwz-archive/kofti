b2c9deec71a22277d81306f206affb74ffe1591a
Baris Ceviz <me@barisceviz.com>
2019-06-29 23:34:10 +0300

    Moved files

---
+++ src/Kofti.Manager/Migrations/20190629173955_InitDb.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Kofti.Manager.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Configs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(nullable: true),
+                    Key = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    ApplicationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Configs_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configs_ApplicationId",
+                table: "Configs",
+                column: "ApplicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configs");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
---
+++ src/Kofti.Manager/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Kofti.Manager.Data;
+using Kofti.Manager.Infrastructure;
+using Kofti.Models;
+using Kofti.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kofti.Manager
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.Configure<KoftiOptions>(Configuration.GetSection("Kofti"));
+
+            services.AddDbContext<KoftiDbContext>(
+                options => { options.UseNpgsql(Configuration.GetConnectionString("DbConnection")); },
+                ServiceLifetime.Singleton);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new CustomViewLocator());
+            });
+            services.AddAutoMapper();
+            services
+                .AddKofti()
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.InitKoftiServer(async message => await LoadConfig(app, message));
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        async Task LoadConfig(IApplicationBuilder app, KoftiInitMessage message)
+        {
+            //TODO (peacecwz): Refactor here for pretty codes
+            var configService = app.ApplicationServices.GetRequiredService<IConfigService>();
+            var dbContext = app.ApplicationServices.GetRequiredService<KoftiDbContext>();
+            var configs = await dbContext.Configs
+                .Include(x => x.Application)
+                .Where(enttiy =>
+                    enttiy.IsActive && !enttiy.IsDeleted && enttiy.Application.Name == message.ApplicationName)
+                .ToListAsync();
+
+            await configService.PublishAsync(message.ApplicationName,
+                configs.ToDictionary(x => x.Key, x => (object) x.Value));
+        }
+    }
+}
